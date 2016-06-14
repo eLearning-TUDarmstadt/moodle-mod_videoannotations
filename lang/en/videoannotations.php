8c45547c00ee295aac07b607eea6d3cbe7e85165
Steffen Pegenau <steffen.pegenau@hrz.tu-darmstadt.de>
2016-06-14 15:47:33 +0200

    Added lang string for the administration menu

--- lang/en/videoannotations.php
+++ lang/en/videoannotations.php
@@ -22,6 +22,7 @@
  * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+$string['pluginadministration'] = 'Administrate video annotation';
 $string['configdisplayoptions'] = '';
 $string['displayoptions'] = '';
 $string['displayselect'] = '';
